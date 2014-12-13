f5ee1d5de22131b29767e9bcaeef52b1711f425f
Julien Bergner <julien@bergner.fr>
2014-12-13 02:47:51 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     resource :clean_data
-    match "destroy_data" => "clean_data#destroy_data"
+    match "destroy_data" => "clean_data#destroy_data" via: [:get, :post]
   end
 end
